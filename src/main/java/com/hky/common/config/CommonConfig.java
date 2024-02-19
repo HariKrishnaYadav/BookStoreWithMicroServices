973f23c5c031268215c6de8b2c17792b98e5b30f
HariKrishnaYadav <Harikrishna.bhuit15@gmail.com>
2024-02-19 17:36:38 +0530

    first commit

---
+++ src/main/java/com/hky/common/config/CommonConfig.java
@@ -0,0 +1,14 @@
+package com.hky.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class CommonConfig {
+
+//    @Bean
+//    DozerBeanMapper dozerBeanMapper(){
+//        DozerBeanMapper dozerBeanMapper = new DozerBeanMapper();
+//    }
+
+}
---
+++ src/main/java/com/hky/common/exception/RunTimeExceptionPlaceHolder.java
@@ -0,0 +1,9 @@
+package com.hky.common.exception;
+
+
+public class RunTimeExceptionPlaceHolder extends RuntimeException {
+
+  public RunTimeExceptionPlaceHolder(String message) {
+    super(message);
+  }
+}
